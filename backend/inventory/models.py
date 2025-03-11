79a459f7c5bf42a7b196cc3e41bbbfd5d100ca79
MesutEmpire <samwainaina01@gmail.com>
2025-03-12 02:44:38 +0300

    Create the db models

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    """Represent the category of inventory items"""
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+class Supplier(models.Model):
+    """Represents suppliers providing inventory items."""
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    address = models.TextField(blank=True,null=True)
+
+
+
+class Item(models.Model):
+
+    """Represents individual inventory items."""
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='items')
+    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True, blank=True, related_name="items")
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f"{self.name} - {self.quantity} in stock"
+
+class StockMovement(models.Model):
+    """Tracks stock movement (additions & removals)."""
+    STOCK_IN = "IN"
+    STOCK_OUT = "OUT"
+
+    MOVEMENT_CHOICES = [
+        (STOCK_IN, "Stock In"),
+        (STOCK_OUT, "Stock Out"),
+    ]
+
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="movements")
+    movement_type = models.CharField(max_length=3, choices=MOVEMENT_CHOICES)
+    quantity = models.PositiveIntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.movement_type} {self.quantity} of {self.item.name}"
