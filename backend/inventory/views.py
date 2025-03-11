34936732fc3c9881ba062f6cac4121e5625345ae
MesutEmpire <samwainaina01@gmail.com>
2025-03-12 02:44:56 +0300

    Create the views apis

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ItemSerializer,StockMovementSerializer
+from .models import Item,StockMovement
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+      API endpoint for managing inventory items.
+      """
+    queryset = Item.objects.all().order_by('-created_at')
+    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class StockMovementViewSet(viewsets.ModelViewSet):
+    """
+      API endpoint for tracking stock movements (Stock In & Stock Out).
+      """
+    queryset = StockMovement.objects.all().order_by('-timestamp')
+    serializer_class = StockMovementSerializer
+    permission_classes = [IsAuthenticated]
