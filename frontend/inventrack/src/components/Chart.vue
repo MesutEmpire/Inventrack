208ee845d034fe14503e5138f523a9f470e86b3a
MesutEmpire <samwainaina01@gmail.com>
2025-03-15 00:10:19 +0300

    Add the chart code

---
+++ frontend/inventrack/src/components/Chart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="h-64">
+    <Bar v-if="chartType === 'bar'" :data="chartData" :options="chartOptions" />
+    <Pie v-else-if="chartType === 'pie'" :data="chartData" :options="chartOptions" />
+    <Line v-else-if="chartType === 'line'" :data="chartData" :options="chartOptions" />
+    <Doughnut v-else-if="chartType === 'doughnut'" :data="chartData" :options="chartOptions" />
+  </div>
+</template>
+
+<script setup>
+import {  Pie, Line, Doughnut } from "vue-chartjs";
+import { Chart, registerables } from "chart.js";
+
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+
+defineProps({
+  chartData: Object,
+  chartOptions: Object,
+  chartType: {
+    type: String,
+    default: "bar"
+  }
+});
+</script>
+
