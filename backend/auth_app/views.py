11aee53618df573303dca107f67a91fcb4fa8ded
MesutEmpire <samwainaina01@gmail.com>
2025-03-15 16:19:19 +0300

    Update the item viewset

--- backend/auth_app/views.py
+++ backend/auth_app/views.py
@@ -8,6 +8,7 @@ from rest_framework.decorators import api_view, permission_classes
 from .models import User
 from inventory.utils.response import CustomResponse
 from .serializers import UserSerializer,RegisterSerializer,LoginSerializer
+from django.contrib.auth import logout
 
 class RegisterView(APIView):
     """
@@ -50,11 +51,15 @@ class LogoutView(APIView):
     def post(self, request):
         try:
             refresh_token = request.data["refresh"]
+            if not refresh_token:
+                return CustomResponse(success=False, message="Refresh token is required", status_code=status.HTTP_400_BAD_REQUEST)
+
             token = RefreshToken(refresh_token)
             token.blacklist()
             logout(request)
-            return CustomResponse(success=True, message="Successfully logged out", status=status.HTTP_200_OK)
-        except Exception:
+            return CustomResponse(success=True, message="Successfully logged out", status_code=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
             return CustomResponse(success=False, message="Invalid token", status_code=status.HTTP_400_BAD_REQUEST)
 
 
--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,16 +1,101 @@
 from rest_framework import viewsets
-from .serializers import ItemSerializer,StockMovementSerializer
-from .models import Item,StockMovement
+from .serializers import ItemSerializer,StockMovementSerializer,CategorySerializer,SupplierSerializer
+from .models import Item,StockMovement,Supplier,Category
 from rest_framework.permissions import IsAuthenticated
-
+from .utils.response import CustomResponse
+from rest_framework import status
 
 class ItemViewSet(viewsets.ModelViewSet):
     """
-      API endpoint for managing inventory items.
-      """
-    queryset = Item.objects.all().order_by('-created_at')
+    API endpoint for managing inventory items.
+    """
+    queryset = Item.objects.select_related('supplier', 'category').order_by('-created_at')
     serializer_class = ItemSerializer
     permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+            queryset = self.get_queryset()
+            serializer = self.get_serializer(queryset, many=True)
+            return CustomResponse(
+                success=True,
+                message="Items retrieved successfully",
+                data=serializer.data,
+                status_code=status.HTTP_200_OK
+            )
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return CustomResponse(
+            success=True,
+            message="Item retrieved successfully",
+            data=serializer.data,
+            status_code=status.HTTP_200_OK
+        )
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(
+                success=True,
+                message="Item created successfully",
+                data=serializer.data,
+                status_code=status.HTTP_201_CREATED
+            )
+        return CustomResponse(
+            success=False,
+            message="Validation failed",
+            data=serializer.errors,
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(
+                success=True,
+                message="Item updated successfully",
+                data=serializer.data,
+                status_code=status.HTTP_200_OK
+            )
+        return CustomResponse(
+            success=False,
+            message="Validation failed",
+            data=serializer.errors,
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(
+                success=True,
+                message="Item partially updated successfully",
+                data=serializer.data,
+                status_code=status.HTTP_200_OK
+            )
+        return CustomResponse(
+            success=False,
+            message="Validation failed",
+            data=serializer.errors,
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return CustomResponse(
+            success=True,
+            message="Item deleted successfully",
+            status_code=status.HTTP_204_NO_CONTENT
+        )
+
+
+
 
 
 class StockMovementViewSet(viewsets.ModelViewSet):
@@ -20,3 +105,157 @@ class StockMovementViewSet(viewsets.ModelViewSet):
     queryset = StockMovement.objects.all().order_by('-timestamp')
     serializer_class = StockMovementSerializer
     permission_classes = [IsAuthenticated]
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    """
+      API endpoint for suppliers
+      """
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+            queryset = self.get_queryset()
+            serializer = self.get_serializer(queryset, many=True)
+            return CustomResponse(
+                success=True,
+                message="Items retrieved successfully",
+                data=serializer.data,
+                status_code=status.HTTP_200_OK
+            )
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return CustomResponse(
+            success=True,
+            message="Item retrieved successfully",
+            data=serializer.data,
+            status_code=status.HTTP_200_OK
+        )
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(
+                success=True,
+                message="Item created successfully",
+                data=serializer.data,
+                status_code=status.HTTP_201_CREATED
+            )
+        return CustomResponse(
+            success=False,
+            message="Validation failed",
+            data=serializer.errors,
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(
+                success=True,
+                message="Item updated successfully",
+                data=serializer.data,
+                status_code=status.HTTP_200_OK
+            )
+        return CustomResponse(
+            success=False,
+            message="Validation failed",
+            data=serializer.errors,
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return CustomResponse(
+            success=True,
+            message="Item deleted successfully",
+            status_code=status.HTTP_204_NO_CONTENT
+        )
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+      API endpoint for categories.
+      """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+            queryset = self.get_queryset()
+            serializer = self.get_serializer(queryset, many=True)
+            return CustomResponse(
+                success=True,
+                message="Items retrieved successfully",
+                data=serializer.data,
+                status_code=status.HTTP_200_OK
+            )
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return CustomResponse(
+            success=True,
+            message="Item retrieved successfully",
+            data=serializer.data,
+            status_code=status.HTTP_200_OK
+        )
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(
+                success=True,
+                message="Item created successfully",
+                data=serializer.data,
+                status_code=status.HTTP_201_CREATED
+            )
+        return CustomResponse(
+            success=False,
+            message="Validation failed",
+            data=serializer.errors,
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(
+                success=True,
+                message="Item updated successfully",
+                data=serializer.data,
+                status_code=status.HTTP_200_OK
+            )
+        return CustomResponse(
+            success=False,
+            message="Validation failed",
+            data=serializer.errors,
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        """Override delete to support bulk deletion"""
+        item_id = kwargs.get('pk')
+        if item_id:
+            return super().destroy(request, *args, **kwargs)
+
+        item_ids = request.data.get("ids", [])  # Expecting a list of item IDs in request body
+        if not item_ids:
+            return CustomResponse(success=False, message="No item IDs provided", status_code=status.HTTP_400_BAD_REQUEST)
+
+        items = Item.objects.filter(id__in=item_ids)
+        deleted_count, _ = items.delete()
+
+        return CustomResponse(success=True, message=f"{deleted_count} item(s) deleted", status_code=status.HTTP_200_OK)
+
+
+
