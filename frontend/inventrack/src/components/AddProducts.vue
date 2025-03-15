85e94de7b68d25979cc48e8cc4d49ec80512e149
MesutEmpire <samwainaina01@gmail.com>
2025-03-15 16:21:45 +0300

    Create add items and update item

--- frontend/inventrack/src/components/AddProducts.vue
+++ frontend/inventrack/src/components/AddProducts.vue
@@ -1,69 +1,116 @@
 <template>
-  <div class="max-w-2xl mx-auto bg-white p-16">
-    <div class="p-4 rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8">
-      <form @submit.prevent="store.addNewProduct">
-        <div class="grid gap-6">
-          <div class="relative md:mx-6">
+  <div class="flex lg:max-w-2xl mx-auto md:max-w-xl sm:max-w-lg p-7 lg:p-16 relative items-center justify-center">
+    <div class="flex flex-col gap-y-5 p-4 rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 w-full relative">
+      <div class="flex justify-center text-2xl lg:text-4xl font-bold ">
+        {{id ? "Update" : "Add"}} Inventory Item
+      </div>
+      <form @submit.prevent="id ? store.updateInventoryItem(id) : store.addNewInventoryItem()" class="flex flex-col gap-y-5">
+        <div class="flex flex-row items-center flex-wrap md:flex-nowrap gap-x-4 gap-y-3">
+          <div class="relative  w-full relative ">
             <input
                 type="text"
-                id="food_title"
-                v-model="store.productForm.title"
+                id="name"
+                v-model="store.inventoryForm.name"
                 class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
             />
             <label
-                for="food_title"
+                for="name"
                 class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
             >
-              Food Title</label
+              Name</label
             >
           </div>
-          <div class="relative md:mx-6">
+          <div class="relative  w-full relative ">
             <input
                 type="number"
-                id="food_price"
-                v-model="store.productForm.price"
+                id="quantity"
+                v-model="store.inventoryForm.quantity"
                 class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
             />
             <label
-                for="food_price"
+                for="quantity"
                 class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
             >
-              Price</label
+              Quantity</label
             >
           </div>
-          <div class="relative md:mx-6">
+        </div>
+        <div class="flex flex-row items-center flex-wrap md:flex-nowrap gap-x-4 gap-y-3">
+
+        <div class="relative  w-full relative ">
             <select
                 type="number"
-                id="food_category"
-                v-model="store.productForm.category"
+                id="supplier"
+                v-model="store.inventoryForm.supplier"
                 class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
             >
               <option disabled selected>Choose a category</option>
-              <option value="Men's Clothing">Men's Clothing</option>
-              <option value="Women's Clothing">Women's Clothing</option>
-              <option value="Electronics">Electronics</option>
-              <option value="Jewelry">Jewelry</option>
+              <option v-for="supplier in store.getSuppliers" :value="supplier.id">{{supplier.name}}f</option>
             </select>
             <label
-                for="food_category"
+                for="supplier"
+                class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+            >
+              Supplier</label
+            >
+          </div>
+          <div class="relative  w-full relative ">
+            <select
+                type="number"
+                id="category"
+                v-model="store.inventoryForm.category"
+                class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+            >
+              <option disabled selected>Choose a category</option>
+<!--              <option value="Men's Clothing">Men's Clothing</option>-->
+<!--              <option value="Women's Clothing">Women's Clothing</option>-->
+<!--              <option value="Electronics">Electronics</option>-->
+<!--              <option value="Jewelry">Jewelry</option>-->
+
+              <option v-for="category in store.getCategories" :value="category.id">{{category.name}}</option>
+            </select>
+            <label
+                for="category"
                 class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
             >
               Category</label
             >
           </div>
+        </div>
+        <div class="flex flex-row items-center flex-wrap md:flex-nowrap gap-x-4 gap-y-3">
 
-          <div class="relative md:mx-6">
+        <div class="relative  w-full relative ">
+            <input
+                type="number"
+                id="price"
+                v-model="store.inventoryForm.price"
+                class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+            />
+            <label
+                for="price"
+                class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+            >
+              Price</label
+            >
+          </div>
+        </div>
+
+          <div class="relative  w-full relative ">
             <textarea
                 type="text"
                 rows="3"
                 id="food_description"
-                v-model="store.productForm.description"
+                v-model="store.inventoryForm.description"
                 class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
@@ -75,62 +122,9 @@
               Description</label
             >
           </div>
-          <div class="mb-4">
-            <div class="flex justify-center items-center md:mx-6">
-              <label
-                  for="food-img"
-                  class="flex flex-col justify-center items-center w-full h-full bg-white rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-              >
-                <div
-                    class="flex flex-col justify-center items-center pt-5 pb-6"
-                >
-                  <svg
-                      class="mb-3 w-10 h-10 text-gray-400"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                    ></path>
-                  </svg>
-                  <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span class="font-semibold"
-                    >Click to upload Food Cover Photo</span
-                    >
-                    or drag and drop
-                  </p>
-                  <p class="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG (MAX. 800x400px)
-                  </p>
-                </div>
-                <input
-                    id="food-img"
-                    type="file"
-                    class="hidden"
-                    @input="event => store.productForm.img = event.target.files[0]">
-                <div
-                    v-if="store.productForm.img != ''"
-                    class="aspect-w-2 aspect-h-3 rounded-lg bg-white overflow-hidden sm:col-span-4 lg:col-span-5"
-                >
-                  <img
-                      :src="store.productForm.img"
-                      :alt="store.productForm.title"
-                      class="object-center object-cover"
-                  />
-                </div>
-              </label>
-            </div>
-          </div>
-        </div>
-
         <button
             type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm lg:text-base w-full sm:w-auto px-5 py-2.5 text-center"
+            class="text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:outline-none focus:ring-pink-500 font-medium rounded-lg text-sm lg:text-base w-full sm:w-auto px-5 py-2.5 text-center "
         >
           Submit
         </button>
@@ -138,8 +132,25 @@
     </div>
   </div>
 </template>
-<script setup lang="ts">
+<script setup>
 import {InventoryStore} from '@/stores/inventoryStore'
+import {computed, watchEffect} from "vue";
+import {useRoute} from "vue-router";
 const store = InventoryStore()
+
+const route = useRoute();
+
+const suppliers = computed(() => store.getSuppliers);
+const categories = computed(() => store.getCategories);
+
+const id = parseInt(route.params?.id);
+
+watchEffect(()=>{
+  store.fetchCategories()
+  store.fetchSuppliers()
+  store.setInventoryForm(id);
+
+})
+
 
 </script>
